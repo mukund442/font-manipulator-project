cede93daa68b9c053a79945e2ee1f38eee23fa52
mukund442 <92722963+mukund442@users.noreply.github.com>
2022-09-01 20:35:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,12 +9,33 @@ function setup(){
     poseNet.on('pose', gotPosses);
 }
 
-function modelLoaded(){
-    console.log('model Loaded');
+function draw(){
+    background('#A9E5EF');
+    fill('##D0EFA9');
+    stroke('##D0EFA9')
+    text('Mukund', 50, 40);
 }
 
-function gotPosses(results){
-    if(results.lenght>0){
+function modelLoaded(){
+    console.log('moidel Loaded');
+}
+
+function gotPoses(results){
+    if(results.length>0){
         console.log(results);
+        nosex=result[0].pose.nose.x;
+        nosey=result[0].pose.nose.y;
+        console.log("nose x:"+nosex+"nose y:"+nosey)
+
+        rightwristx=result[0].pose.rightwrist.x;
+        leftwristx=result[0].pose.leftwrist.x;
+        difference=float(leftwristx-rightwristx);
+        console.log("rightwrist x:"+rightwristx+"leftwrist"+leftwristx+"difference:"+difference)
     }
 }
+
+nosex=0;
+nosey=0;
+rightwristx=0;
+leftwristx=0;
+difference=0;
